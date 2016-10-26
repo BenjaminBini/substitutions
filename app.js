85e76a518de0dec97eaf2fa4d9b08a36b8762b5b
BenjaminBini <benjamin.bini@gmail.com>
2016-10-26 08:35:40 +0200

    Prevent bot to reply to itself

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ const stream = T.stream('user');
 
 // When a tweet is received
 stream.on('tweet', function (tweet) {
-    console.log('New tweet received: ');
-    console.log(tweet.text)
     var newTweet = tweet.text;
     // Loop through the substitutions map and replace accordingly
     for (var [key, value] of substitutions.entries()) {
@@ -36,11 +34,11 @@ stream.on('tweet', function (tweet) {
             console.log(key + ' => ' + value);
         }
     }
-    // Tweet a reply if a modification happened
-    if (newTweet != tweet.text) {
-        console.log('Tweet has been modified: ');
-        console.log(newTweet);
-        var newTweet = '.@' + tweet.user.screen_name + ' ' + newTweet;
+    // Tweet a reply if a modification happened and if the tweet is not from substitutions_ account
+    if (newTweet != tweet.text && tweet.user.screen_name !== 'substitutions_') {
+        console.log('@' + tweet.user.screen_name + ' ' + tweet.text);
+        newTweet = '.@' + tweet.user.screen_name + ' ' + newTweet;
+        console.log('=> ' + newTweet);
         if (newTweet.length <= 140) {
             T.post('statuses/update', {
                 in_reply_to_status_id: tweet.id_str,
@@ -55,22 +53,6 @@ stream.on('tweet', function (tweet) {
         } else {
             console.log('Tweet too long (' + newTweet.length + ' characters)');
         }
-        /*
-        // Tweet with a leading dot to display the tweet in everyone's timeline
-        if (newTweet.length <= 141) {
-            T.post('statuses/update', {
-                status: '.' + newTweet
-            }, function (err, data, response) {
-                if (err) {
-                    console.dir(err);
-                }
-                console.log('>>> Mention with a leading dot sent');
-                console.log();
-            })
-        } else {
-            console.log('Tweet too long to send a "dot mention" (' + newTweet.length + ' characters)');
-        }
-        */
     }
     console.log();
 });
