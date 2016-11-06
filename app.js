0c41af29d085071b8a0ed30678b0b706c4c405d2
BenjaminBini <benjamin.bini@gmail.com>
2016-11-06 02:17:03 +0100

    Post only the best tweet (with the most substitutions) every five minutes

--- app.js
+++ app.js
@@ -22,41 +22,59 @@ const T = new Twit({
 // Initialize stream API
 const stream = T.stream('user');
 
-// We tweet not more than every 5 minutes
-var lastTweetTime = new Date();
+/**
+ * We're going to post the tweet with the most substitutions in the last substitutions
+ */
 
-// When a tweet is received
+// We store the tweet with the most substitutions and the number of substitutions
+var bestTweet;
+var bestTweetReplyTo;
+var bestSubstitutionsCount = 0;
+
+// Tweet every 5 minutes
+setInterval(tweet, 1000 * 60 * 5);
+
+function tweet() {
+    if (!!bestTweet) {
+        T.post('statuses/update', {
+            in_reply_to_status_id: bestTweetReplyTo,
+            status: bestTweet
+        }, function (err, data, response) {
+            if (err) {
+                console.dir(err);
+            }
+            console.log('>>> Tweet sent');
+            console.log();
+        });
+    }
+    // Reset the best tweet data
+    bestTweet = undefined;
+    bestTweetReplyTo = undefined;
+    bestSubstitutionsCount = 0;
+}
+
+// When a tweet is received, we save the data necessary to post it if this is the one with the most substitutions
 stream.on('tweet', function (tweet) {
     var newTweet = tweet.text;
+    var substitutionsCount = 0;
     // Loop through the substitutions map and replace accordingly
     for (var [key, value] of substitutions.entries()) {
         const regEx = new RegExp('\\b' + key + '\\b', 'ig');
         let previousTweet = newTweet;
         newTweet = newTweet.replace(regEx, value.toLowerCase());
         if (newTweet != previousTweet) {
-            console.log(key + ' => ' + value);
+            substitutionsCount++;
         }
     }
-    // Tweet a reply if a modification happened and if the tweet is not from substitutions_ account
-    if (newTweet != tweet.text && tweet.user.screen_name !== 'substitutions_') {
-        console.log('@' + tweet.user.screen_name + ' ' + tweet.text);
+    // Save the tweet to post if the tweet is valid, not from substitutions_ account and the one with the most substitutions
+    if (substitutionsCount > bestSubstitutionsCount 
+            && newTweet != tweet.text 
+            && tweet.user.screen_name !== 'substitutions_') {
         newTweet = '.@' + tweet.user.screen_name + ' ' + newTweet;
-        console.log('=> ' + newTweet);
-        if (newTweet.length <= 140 && new Date() - lastTweetTime > 1000 * 60 * 5) {
-            T.post('statuses/update', {
-                in_reply_to_status_id: tweet.id_str,
-                status: newTweet
-            }, function (err, data, response) {
-                if (err) {
-                    console.dir(err);
-                }
-                lastTweetTime = new Date();
-                console.log('>>> Tweet sent');
-                console.log();
-            });
-        } else {
-            console.log('Tweet too long (' + newTweet.length + ' characters)');
+        if (newTweet.length <= 140) {
+            bestTweetReplyTo = tweet.id_str;
+            bestTweet = newTweet;
+            bestSubstitutionsCount = substitutionsCount;
         }
-        console.log();
     }
 });
